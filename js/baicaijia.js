be45f9e2a3f85df0fe85c53d61611d05bbb7e661
lvming <lv@qq.com>
2019-03-05 11:59:48 +0800

    完整版1.0

--- js/baicaijia.js
+++ js/baicaijia.js
@@ -8,7 +8,6 @@ $(function(){
            console.log( info );
            var htmlstr = template('baiTpl',info);
            $('.nav').html(htmlstr);
-  console.log( $('.nav li'));
 
         }
     })
@@ -26,6 +25,31 @@ $(function(){
             console.log(info );
           var htmlstr = template('contentTpl',info);
           $('.tab-content').html(htmlstr);
+
+          //设置区域滚动;
+        var ul = document.querySelector('.nav');
+        //   var lis = $('.nav li');
+          console.log(ul);
+          var width =10;
+          $(".nav li").each(function(index,ele){
+            width += ele.offsetWidth;
+             
+          })
+          console.log(width);
+        //   设置宽度
+          ul.style.width = width + "px";
+
+      
+        //   // 设置 ul
+        //   ul.style.width = ulWidth + "px";
+        
+        
+          // 初始化要放在动态设置 宽度的 下面, 保证计算准确性
+          new IScroll("#wrapper", {
+            scrollX: true,
+            scrollY: false
+          });
+        
             }
         })
     }
@@ -51,4 +75,19 @@ $(function(){
        })
       
     })
+
+    //滚动,初始化
+     
+            // new IScroll('#wrapper',{  
+            //     mouseWheel : true,      //鼠标滚轮支持  
+               
+            //     scrollY : false,         //滚动方向（垂直）  
+            //     scrollX : true,         //滚动方向（水平）  
+            //     bounce : true,          //边界时的反弹动画，默认true  
+                  
+            // });  
+         
+   
+    
+
 })
--- js/couponproduct.js
+++ js/couponproduct.js
@@ -9,9 +9,11 @@ $(function(){
         // str.slice(start) 从start 开始到结尾;
         str = str.slice(1);
         var arr = str.split('=');
-        return arr[1]
+        return arr
     }
-   var id = getSearch();
+    var content = getSearch()[0];
+    $('.header').text(content);
+   var id = getSearch()[1];
   console.log(id);
   
    $.ajax({
@@ -25,6 +27,82 @@ $(function(){
          console.log( info );
         var htmlstr = template('yTpl',info);
         $('.product ul').html(htmlstr);
+
+
+
+
+
+
+          //实验产品
+        // console.log($('.left img'));
+        var imgs = $('.left img');
+       var arr = [];
+      imgs.each(function(index,v){
+          arr.push(v.src);
+      })
+      $('.btn img').attr('src',arr[1]);
+    //   console.log(arr);
+
+      var nows = 0
+      $('.btn .next').on('click',function(){
+          console.log(1);
+          nows++;
+          if(nows >arr.length-1){
+              nows =0
+          }
+         
+           $('.btn img').attr('src',arr[nows]);
+      })
+      $('.btn .pre').on('click',function(){
+        console.log(2);
+        
+        nows--;
+        if(nows < 0){
+            nows =arr.length-1;
+        }
+         $('.btn img').attr('src',arr[nows]);
+    })
+        //点击显示
+        $('ul').on('click','.left a',function(){
+            var imgid = $(this).data('id')
+
+            console.log(arr[2]);
+           
+            $('.btn img').attr('src',arr[imgid]);
+           show();
+        })
+        //点击隐藏
+        $('lbOverlay').on('click',function(){
+            closeDiv();
+        })
+    
     }
+      
 })
+
+
+//调用方法
+function show(){
+    $(".lbOverlay").css({"height":window.screen.availHeight});
+    $(".lbOverlay").show();
+ 
+    var st=$(document).scrollTop(); //页面滑动高度
+    var objH=$(".hidden_pro_au").height();//浮动对象的高度
+    var ch=$(window).height();//屏幕的高度  
+    var objT=Number(st)+(Number(ch)-Number(objH))/2;   //思路  浮动高度+（（屏幕高度-对象高度））/2
+    $(".hidden_pro_au").css("top",objT);
+     
+    var sl=$(document).scrollLeft(); //页面滑动左移宽度
+    var objW=$(".hidden_pro_au").width();//浮动对象的宽度
+    var cw=$(window).width();//屏幕的宽度  
+    var objL=Number(sl)+(Number(cw)-Number(objW))/2; //思路  左移浮动宽度+（（屏幕宽度-对象宽度））/2
+    $(".hidden_pro_au").css("left",objL);
+    $(".hidden_pro_au").slideDown("20000");//这里显示方式多种效果
+}
+function closeDiv(){
+    $(".lbOverlay").hide();
+    $(".hidden_pro_au").hide();
+}
 })
+
+ 
